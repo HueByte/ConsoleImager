ffb47b27aff2816970452ac1def8d4329d0468a6
HueByte <ihuebyte@gmail.com>
2022-09-21 21:15:26 +0200

    Some upgrades to arguments

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,10 @@
-﻿using System.Diagnostics;
-using SkiaSharp;
+﻿using SkiaSharp;
 
-// Stopwatch watch = new(); watch.Start();
 HttpClient client = new();
 
 var imageUrl = args.Length != 0 ? args[0] : "https://preview.redd.it/653ztm9yjtg31.jpg?auto=webp&s=da7aeb94db7f956bea0f2a4b1543af4714cbadc4";
-int width = args.Length != 0 ? Convert.ToInt32(args[1]) : 100;
-int height = Convert.ToInt32(width * 0.5);
+int width = !(args.Length <= 1) ? Convert.ToInt32(args[1]) : 100;
+int height = !(args.Length <= 2) ? Convert.ToInt32(args[2]) : Convert.ToInt32(width * 0.5);
 
 var imageBytes = await client.GetByteArrayAsync(imageUrl);
 var bitmap = SKBitmap.Decode(imageBytes);
@@ -40,10 +38,8 @@ for (int h = 0; h < outputArr.GetLength(0); h++)
     }
 
     Console.Write(output);
+    await Task.Delay(100);
 }
-
-// watch.Stop();
-// Console.WriteLine(watch.ElapsedMilliseconds);
 
 static string GetPrinter(SKColor color)
 {
