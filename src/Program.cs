4c1d80a5239489419e95e75eb2d75a0e7dd401b6
HueByte <ihuebyte@gmail.com>
2022-09-21 20:55:07 +0200

    Performance improvements

--- src/Program.cs
+++ src/Program.cs
@@ -1,8 +1,10 @@
-﻿using SkiaSharp;
+﻿using System.Diagnostics;
+using SkiaSharp;
 
+// Stopwatch watch = new(); watch.Start();
 HttpClient client = new();
 
-var imageUrl = args[0];
+var imageUrl = args.Length != 0 ? args[0] : "https://preview.redd.it/653ztm9yjtg31.jpg?auto=webp&s=da7aeb94db7f956bea0f2a4b1543af4714cbadc4";
 int width = args.Length != 0 ? Convert.ToInt32(args[1]) : 100;
 int height = Convert.ToInt32(width * 0.5);
 
@@ -12,17 +14,36 @@ var bitmap = SKBitmap.Decode(imageBytes);
 SKBitmap resizedImage = new(width, height);
 bitmap.ScalePixels(resizedImage, SKFilterQuality.Low);
 
-for (int h = 0; h < resizedImage.Height; h++)
+ParallelOptions options = new()
 {
-    string line = "";
-    for (int w = 0; w < resizedImage.Width; w++)
+    MaxDegreeOfParallelism = Environment.ProcessorCount
+};
+
+string[,] outputArr = new string[resizedImage.Height, resizedImage.Width + 1];
+Parallel.For(0, resizedImage.Height, options, (heightIndex) =>
+{
+    for (int widthIndex = 0; widthIndex < resizedImage.Width; widthIndex++)
     {
-        var pixel = resizedImage.GetPixel(w, h);
-        line += GetPrinter(new SKColor(pixel.Red, pixel.Green, pixel.Blue));
+        outputArr[heightIndex, widthIndex] = GetPrinter(resizedImage.GetPixel(widthIndex, heightIndex));
     }
-    Console.WriteLine(line);
-    await Task.Delay(15);
+
+    outputArr[heightIndex, resizedImage.Width] = Environment.NewLine;
+});
+
+string output = "";
+for (int h = 0; h < outputArr.GetLength(0); h++)
+{
+    output = "";
+    for (int w = 0; w < outputArr.GetLength(1); w++)
+    {
+        output += outputArr[h, w];
+    }
+
+    Console.Write(output);
 }
+
+// watch.Stop();
+// Console.WriteLine(watch.ElapsedMilliseconds);
 
 static string GetPrinter(SKColor color)
 {
